5ab04a11a56f920b244319586fe92c1af8d5bc7b
trendtao <1002978928@qq.com>
2018-08-09 13:58:20 +0800

    创建webpack环境依赖

--- Vue-webpack/src/main.js
+++ Vue-webpack/src/main.js
@@ -1,6 +1,10 @@
 import $ from "jquery"
 
+import "./css/index.css"
+import "./css/index.less"
+
 $(function(){
-    $("ul li:odd").css("background-color","red");
-    $("ul li:even").css("background-color","green");
+    $("ul li:odd").css("background-color","#112233");
+    $("ul li:even").css("background-color","#123ddd");
+    $("ul li").css("color","white");
 })
--- Vue-webpack/webpack.config.js
+++ Vue-webpack/webpack.config.js
@@ -1,6 +1,8 @@
 //配置webpack的属性
 
 const path = require("path")
+const webpack = require("webpack")
+const htmlWebpackPlugin = require("html-webpack-plugin")
 
 //向webpack暴露对象
 module.exports={
@@ -9,4 +11,28 @@ module.exports={
         path:path.join(__dirname,"./dist"),
         filename:"bundle.js",
     },
+    //第二种自动部署方式，不过不推荐，很麻烦，可以在pageage.json里面配置
+    devServer:{
+        open:true,
+        port:2888,
+        contentBase:"src",
+        hot:true  //启动热更新，需要引入webpack模块，以及配置plugins插件
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        //这里引用 html-webpack-plugin 插件，因此可以除去加载页面引用的样式和脚本
+        new htmlWebpackPlugin({
+            template:path.join(__dirname,"./src/index.html"),
+            filename:"index.html"
+        })
+    ],
+    module:{
+        rules:[
+            {test:/\.css$/, use:['style-loader','css-loader']},
+            {test:/\.less$/, use:['style-loader','css-loader','less-loader']},
+        ]
+    }
+
+
+
 } 
