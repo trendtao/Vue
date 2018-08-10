264b035aa90e4ce056a38a0c96b28ce29413e1d7
trendtao <1002978928@qq.com>
2018-08-10 17:20:55 +0800

    添加插件

--- Vue-webpack/src/main.js
+++ Vue-webpack/src/main.js
@@ -2,9 +2,16 @@ import $ from "jquery"
 
 import "./css/index.css"
 import "./css/index.less"
+import "bootstrap/dist/css/bootstrap.css"
 
 $(function(){
     $("ul li:odd").css("background-color","#112233");
     $("ul li:even").css("background-color","#123ddd");
     $("ul li").css("color","white");
 })
+
+class Person{
+    static children = {age:19,name:"涛哥"}
+}
+
+console.log(Person.children)
--- Vue-webpack/webpack.config.js
+++ Vue-webpack/webpack.config.js
@@ -5,31 +5,37 @@ const webpack = require("webpack")
 const htmlWebpackPlugin = require("html-webpack-plugin")
 
 //向webpack暴露对象
-module.exports={
-    entry: path.join(__dirname,"./src/main.js"),
-    output:{
-        path:path.join(__dirname,"./dist"),
-        filename:"bundle.js",
+module.exports = {
+    entry: path.join(__dirname, "./src/main.js"),
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "bundle.js",
     },
     //第二种自动部署方式，不过不推荐，很麻烦，可以在pageage.json里面配置
-    devServer:{
-        open:true,
-        port:2888,
-        contentBase:"src",
-        hot:true  //启动热更新，需要引入webpack模块，以及配置plugins插件
+    devServer: {
+        open: true,
+        port: 2888,
+        contentBase: "src",
+        hot: true  //启动热更新，需要引入webpack模块，以及配置plugins插件
     },
-    plugins:[
+    plugins: [
         new webpack.HotModuleReplacementPlugin(),
         //这里引用 html-webpack-plugin 插件，因此可以除去加载页面引用的样式和脚本
         new htmlWebpackPlugin({
-            template:path.join(__dirname,"./src/index.html"),
-            filename:"index.html"
+            template: path.join(__dirname, "./src/index.html"),
+            filename: "index.html"
         })
     ],
-    module:{
-        rules:[
-            {test:/\.css$/, use:['style-loader','css-loader']},
-            {test:/\.less$/, use:['style-loader','css-loader','less-loader']},
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            //limit(单位：byte)):代表文件大小大于这个数字，就会base64转换
+            //name: 生成的文件名跟自己的文件名一样
+            { test: /\.(png|jpg|gif|jpeg|bpm)$/, use: "url-loader?limit=27118&name=[hash:8]-[name].[ext]" },
+            //匹配字体文件
+            { test: /\.(eot|svg|ttf|woff|woff2)$/, use: "url-loader?name=fonts/[hash:8]-[name].[ext]" },
+            { test: /\.js$/, use: "babel-loader", exclude: /node_modules/ }
         ]
     }
 
